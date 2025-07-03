fac6909d3acc5c3ccdb391c396585e0e89c9093f
Leon Bauer <leon.bauer@uni-oldenburg.de>
2025-07-03 16:33:09 +0200

    Codereinigung

--- training_nace.py
+++ training_nace.py
@@ -5,20 +5,20 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import joblib
 
-# Daten laden
+# Load file
 df = pd.read_csv("training_data.csv")
 
 X_texts = df["Text"]
 y_labels = df["NACE"]
 
-# TF-IDF + Modell trainieren
+# TF-IDF + Model training
 vectorizer = TfidfVectorizer(max_features=5000)
 X = vectorizer.fit_transform(X_texts)
 
 model = LogisticRegression(max_iter=1000)
 model.fit(X, y_labels)
 
-# Speichern
+# Save
 joblib.dump(vectorizer, "models/nace_vectorizer.pkl")
 joblib.dump(model, "models/nace_model.pkl")
 
